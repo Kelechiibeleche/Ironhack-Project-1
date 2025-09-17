d1d9dcff460c8e108c4922d4976f9f77fade5237
kelechi <kelechibeleche@gmail.com>
2025-09-17 17:03:29 +0200

    test

--- question.js
+++ question.js
@@ -1,7 +1,7 @@
 window.QUESTIONS = [
-  { name: "Batman", role: "Hero", img: "images/batman.png" },
-  { name: "Wonder Woman", role: "Hero", img: "images/wonderwoman.png" },
-  { name: "Black Panther", role: "Hero", img: "images/blackpanther.png" },
+  { name: "Batman", role: "Hero", img: "../images/batman.png" },
+  { name: "Wonder Woman", role: "Hero", img: "./images/wonderwoman.png" },
+  { name: "Black Panther", role: "Hero", img: "/images/blackpanther.png" },
   { name: "Green Lantern", role: "Hero", img: "images/greenlantern.png" },
   { name: "Cyborg", role: "Hero", img: "images/cyborg.png" },
   { name: "Joker", role: "Villain", img: "images/Joker.png" },
